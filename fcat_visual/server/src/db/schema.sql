688cb65b639c94d11926c12a72c7d33d5dc27f4b
hailia <hailiassommerville@gmail.com>
2026-03-16 14:04:46 -0400

    chore: db schema

--- fcat_visual/server/src/db/schema.sql
+++ fcat_visual/server/src/db/schema.sql
@@ -1,1 +1,23 @@
 -- CREATE TABLE definitions for: area, grid, work_order, ingestion_logs
+
+CREATE TABLE area (
+    pm_id VARCHAR PRIMARY KEY,
+    description VARCHAR
+);
+
+CREATE TABLE grid (
+    grid_id VARCHAR PRIMARY KEY,
+    x_pos INTEGER,
+    y_pos INTEGER,
+    pm_id VARCHAR FOREIGN KEY
+);
+
+CREATE TABLE work_order (
+    work_order_id VARCHAR PRIMARY KEY,
+    pm_id VARCHAR FOREIGN KEY,
+    status VARCHAR,
+    target_start_date TIMESTAMP,
+    frequency VARCHAR,
+    description VARCHAR
+);
+
